464da1d46e3292969fb5ee77a88a95d058ea3e5e
igiza1213 <igiza1213@gmail.com>
2023-06-13 11:16:29 +0900

    Create 0613.py

---
+++ colab/0613.py
@@ -0,0 +1,52 @@
+# data = input("문자열 입력: ")
+# for i in range(0, len(data)-1):
+#     print(data[i], data[i+1], sep="")
+
+# yoil = "월화수목금토일"
+# print(yoil[::2])
+# print(yoil[::-1])
+
+# a = "10 20 30 40 50"
+# print(a.split(" "))
+# b = "나는 서울로봇고 2학년이야"
+# print(b.split(" "))
+# c = "a:b:c:d"
+# print(c.split(":"))
+
+# print(",".join("abcd"))
+# print("::".join("12345"))
+
+# s = "python programming"
+
+# print("문자열  s의 길이 =",  len(s))
+# print("문자열  s에서 o의 인덱스 =",  s.find("o"))
+# print("문자열  없는 인덱스 =",  s.find("A"))
+# print("문자열  o를 뒤애서 검색한 인덱스 =",  s.rfind("o"))
+# print("문자열  s에서 r의 인덱스 =",  s.index("r"))
+# print("문자열  s에서 n의 개수 =",  s.count("n"))
+# print("문자열  s에서 gram의 인덱스 =",  s.index("gram"))
+# print("문자열  s에서 gram의 개수 =",  s.find("gram"))
+# print("문자열  s에서 gram의 개수 =",  s.count("gram"))
+
+# s = "python programming"
+# print("a" in s)
+# print("z" in s)
+# print("pro" in s)
+# print("x" not in s)
+
+
+s = "good morning. MyLove Kim."
+print(s)
+print(s.upper())
+print(s.lower())
+
+print(s.swapcase())
+print(s.capitalize())
+print(s.title())
+
+
+
+
+
+
+
